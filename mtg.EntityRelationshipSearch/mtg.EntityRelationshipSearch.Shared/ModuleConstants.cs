9a89d01fd0011687f93590fc5864218dbc53e23a
fcknullexception <fcknullexception@mail.ru>
2025-04-16 09:38:09 +0300

    Удалена лишняя константа

--- mtg.EntityRelationshipSearch/mtg.EntityRelationshipSearch.Shared/ModuleConstants.cs
+++ mtg.EntityRelationshipSearch/mtg.EntityRelationshipSearch.Shared/ModuleConstants.cs
@@ -5,11 +5,6 @@ namespace mtg.EntityRelationshipSearch.Constants
 {
   public static class Module
   {
-
-    /// <summary>
-    /// Набор символов, для деления строки в отчете.
-    /// </summary>
-    public const string SeparateCharacters = "!$#)";
-
+    
   }
 }
