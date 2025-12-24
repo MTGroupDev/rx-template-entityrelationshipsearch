ccdb6f2c99b74518456dd703e4e7abd76e16e709
Serenite1337 <vampirpir53@gmail.com>
2025-12-24 16:04:04 +0300

    Собрано и опубликовано на RX 25.3
    Собрано и опубликовано на RX 25.3

---
+++ mtg.EntityRelationshipSearch/mtg.EntityRelationshipSearch.Server/Reports/EntityRelashionshipSearchReport/Queries/CreateTableData_mssql.sql
@@ -0,0 +1,6 @@
+CREATE TABLE {0}
+(ReportSessionId varchar(256) NOT NULL,
+EntityId bigint NOT NULL,
+EntityType nvarchar(max) NOT NULL, 
+Name nvarchar(max) NULL, 
+EntityHyperlink nvarchar(max) NOT NULL)
